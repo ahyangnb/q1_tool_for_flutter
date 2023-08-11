e9582f23a21518ca492a3cdc05e4b90130653f74
q1 <zonggeu@gmail.com>
2023-08-11 22:34:52 +0800

    request model generate

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,190 @@
+import 'package:dart_style/dart_style.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'cURL to Dart Tool',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CurlToDartConverter(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CurlToDartConverter extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CurlToDartConverterState createState() => _CurlToDartConverterState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CurlToDartConverterState extends State<CurlToDartConverter> {
+  String curlCommand = '';
+  String generatedDartCode = '';
 
-  void _incrementCounter() {
+  String _getRequestMethod() {
+    // Extract the request method from the cURL command
+    // Example: -X POST -> POST
+    final RegExp methodRegex = RegExp(r"-X ([A-Z]+)");
+    final match = methodRegex.firstMatch(curlCommand);
+    return match?.group(1)?.toUpperCase() ?? 'GET';
+  }
+
+  String capitalizeFirstLetter(String input) {
+    if (input.isEmpty) return input;
+
+    return input[0].toUpperCase() + input.substring(1);
+  }
+
+  String lowercaseFirstLetter(String input) {
+    if (input.isEmpty) return input;
+
+    return input[0].toLowerCase() + input.substring(1);
+  }
+
+  String _generateClassName(String baseUrl, String methodLowerStr) {
+    Uri uri = Uri.parse(baseUrl);
+
+    // Define a regular expression pattern to match the desired part
+    RegExp regex = RegExp(r'/([^/]+)/([^/]+)/([^/]+)/([^/]+)$');
+
+    RegExpMatch regExpMatch = regex.firstMatch(uri.path)!;
+
+    // Use the first capturing group from the matched result
+    String extractedPart =
+        "${capitalizeFirstLetter(regExpMatch.group(2)!)}${capitalizeFirstLetter(regExpMatch.group(3)!)}";
+
+    return capitalizeFirstLetter(methodLowerStr) + extractedPart;
+  }
+
+  bool pathContainsParam(String path) {
+    RegExp paramPattern = RegExp(
+        r'/[a-f\d-]+$'); // Match UUID-like strings at the end of the path
+    return paramPattern.hasMatch(path);
+  }
+
+  String extractParamValueFromPath(String path) {
+    RegExp paramPattern = RegExp(
+        r'/([a-f\d-]+)$'); // Match and capture UUID-like strings at the end of the path
+    Match? match = paramPattern.firstMatch(path);
+    return match?.group(1) ?? 'No param found';
+  }
+
+  void convertToDartCode() {
+    final RegExp urlRegex = RegExp(r"'(https?://[^']+)'");
+    final RegExp dataRegex = RegExp(r"--data-raw '([^']*)'");
+
+    final RegExpMatch? urlMatch = urlRegex.firstMatch(curlCommand);
+    if (urlMatch == null) {
+      print("urlMatch == null");
+      return;
+    }
+
+    final String baseUrl = urlMatch.group(1)!;
+    final String methodLowerStr = _getRequestMethod().toLowerCase();
+
+    final String className = _generateClassName(baseUrl, methodLowerStr);
+    Uri uri = Uri.parse(baseUrl);
+    String path = uri.path;
+
+    final bool paramFromPath = pathContainsParam(path);
+    if (paramFromPath) {
+      path = "${path.replaceAll(extractParamValueFromPath(path), "")}\$id";
+    }
+
+    final Map<String, dynamic> data = {};
+
+    print("data::${data.toString()}");
+
+    dataRegex.allMatches(curlCommand).forEach((match) {
+      data['data'] = match.group(1)!;
+    });
+    final String requestModelName = '${className}RequestModel';
+
+    final String generatedCode = '''
+class $requestModelName extends BaseRequest {
+  ${paramFromPath ? "final String id;" : ""}
+
+  $requestModelName(${paramFromPath ? "this.id," : ""});
+  
+  @override
+  String url() => '$path';${data.isEmpty ? "" : """
+  
+  @override
+  Map<String, dynamic> toJson() {
+    return {}; 
+  }"""}
+}
+
+Future<ResponseModel> ${lowercaseFirstLetter(className)}(BuildContext? context,{${paramFromPath ? "required final String id," : ""}}) async {
+  return $requestModelName(${paramFromPath?"id":""}).sendApiAction(context, reqType: ReqType.$methodLowerStr)
+      .then((rep) {
+    // Parse the response using appropriate logic
+    // Replace with your code to handle the response
+    return ResponseModel.fromSuccess(rep);
+  }).catchError((e) {
+    return throwError(e);
+  }).onError((error, stackTrace) {
+    return onError(error, stackTrace);
+  });
+}
+''';
+
+    DartFormatter formatter = DartFormatter();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      generatedDartCode = formatter.format(generatedCode);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    curlCommand = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Enter cURL Command',
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  convertToDartCode();
+                },
+                child: Text('Convert to Dart Code'),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Generated Dart Code:',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: SelectableText(generatedDartCode),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
